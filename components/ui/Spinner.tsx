da3a1627e1a63fc56158bd0c9f7e516181821186
knwpound <pound.somparn@gmail.com>
2026-04-06 16:46:25 +0700

    Create Spinner.tsx

---
+++ components/ui/Spinner.tsx
@@ -0,0 +1,7 @@
+export function Spinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-20 h-20 border-10 border-gray-200 border-t-orange-500 rounded-full animate-spin duration-1000" />
+    </div>
+  );
+}
