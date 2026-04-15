02031c56dbc229a1483215e535af04eef9501ee4
knwpound-2 <pound.somparn2547@gmail.com>
2026-04-15 20:40:14 +0700

    Update page.tsx

--- app/player/[slug]/page.tsx
+++ app/player/[slug]/page.tsx
@@ -1,7 +1,30 @@
 import MeditationPlayer from "@/components/ui/home/MeditationPlayer";
 
-export default function PlayerPage(){
-    return(
-        <MeditationPlayer/>
-    )
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function PlayerPage({ 
+  params, 
+  searchParams 
+}: { 
+  params: Promise<{ slug: string }>,
+  searchParams: Promise<{ mood: string }> 
+}) {
+  const { slug } = await params;
+  const { mood } = await searchParams;
+
+  const audioUrl = `${process.env.NEXT_PUBLIC_S3_URL}/${slug}.mp3`;
+  console.log(audioUrl)
+
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <MeditationPlayer 
+        title={mood||"Your Meditation Journey"}
+        audioUrl={audioUrl} 
+      />
+    </main>
+  );
 }
