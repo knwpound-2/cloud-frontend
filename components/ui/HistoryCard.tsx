4b325a8e747b02016b4fbc7c8b4d79d4514befa2
knwpound-2 <pound.somparn2547@gmail.com>
2026-04-15 20:39:36 +0700

    connect: api

--- components/ui/HistoryCard.tsx
+++ components/ui/HistoryCard.tsx
@@ -1,20 +1,25 @@
-import Link from 'next/link'; 
+import { formatTimestamp } from '@/lib/util/formatTimestamp';
+import Link from 'next/link';
 
+export interface MeditationInfo {
+  userId: string;
+  createdAt: string;
+  mood: string;
+  audioUrl: string;
+  userInput: string;
+}
 interface HistoryCardProps {
-  item: {
-    id: string;
-    title: string;
-    album: string;
-    duration: string;
-    image: string;
-  };
+  item: MeditationInfo;
   index: number;
 }
 
 export default function HistoryCard({ item, index }: HistoryCardProps) {
+  const filename = item.audioUrl.split('/').pop() || '';
+  const slug = filename.replace('.mp3', '');
+  const createdAt = formatTimestamp(item.createdAt);
   return (
     <Link
-      href={`/player/${item.id}`}
+      href={`/player/${slug}?mood=${item.mood}`}
       className="group flex items-center justify-between p-2 hover:bg-gray-50 transition-colors rounded-lg border border-transparent hover:border-gray-200"
     >
       <div className="flex items-center gap-4">
@@ -22,26 +27,24 @@ export default function HistoryCard({ item, index }: HistoryCardProps) {
         <span className="text-[10px] font-bold opacity-40">
           {(index + 1).toString().padStart(2, '0')}
         </span>
-        
+
         <img
-          src={item.image}
-          alt={item.title}
-          className="w-10 h-10 object-cover rounded-sm shadow-sm"
+          src={'cover.png'}
+          alt={item.mood}
+          className="w-10 h-10 object-cover rounded-sm"
         />
-        
+
         <div>
           <h3 className="text-[11px] font-bold tracking-tight leading-tight group-hover:text-black">
-            {item.title}
+            {item.mood}
           </h3>
           <p className="text-[9px] font-medium opacity-50 tracking-wider uppercase">
-            {item.album}
+            {createdAt}
           </p>
         </div>
       </div>
 
-      <span className="text-[10px] font-mono opacity-60">
-        {item.duration}
-      </span>
+      <span className="text-[10px] font-mono opacity-60">{item.userId}</span>
     </Link>
   );
 }
